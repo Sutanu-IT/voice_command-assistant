066f5ea7f398a1ce2a57f953e694d491a0f9c515
Sutanu-IT <sutanu040502@gmail.com>
2022-07-06 13:17:59 +0530

    some new function added and code modified

--- voice_command.py
+++ voice_command.py
@@ -1,34 +1,60 @@
-from datetime import datetime
 import speech_recognition as sr
 import pyttsx3 as ps
 import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import webbrowser
 
-listener = sr.Recognizer()
+
 engine=ps.init()
 voices=engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
-engine.setProperty('rate',150)
+engine.setProperty('rate',170)
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+def wishme():
+    hour=int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        talk('good morning')
+        talk('Hello i am alexa please tell me how can i help you')
+    elif hour>=12 and hour<18:
+        talk("Good Afternoon")
+        talk('Hello i am alexa please tell me how can i help you')
+    else:
+        talk("good evening")
+        talk('Hello i am alexa please tell me how can i help you')
+    
+
 def take_command():
     try:
+        listener = sr.Recognizer()
         mic=sr.Microphone()
         with mic as source:
-            talk('Hello i am alexa I am listening to you')
-            voice=listener.listen(source)
-            command=listener.recognize_google(voice)
+            print("\nListening...\n")
+            # listener.pause_threshold=1
+            listener.energy_threshold=200
+            audio=listener.listen(source)
+            print('Recognizing...\n')
+            command=listener.recognize_google(audio)
             command=command.lower()
             if 'alexa' in command:
                 command=command.replace('alexa','')
-                # print(command)
+                print(f"User said : {command}\n")
+
+            elif 'alexa' not in command:
+                print('Alexa said: I am alexa please mention alexa in command')
+                talk('i am alexa please mention alexa in command')
+                take_command()
+                
     except:
-        pass
+        print('Alexa said: Powering off...')
+        talk('Powering off')
+        quit()
+   
     return command
 
 def run_alexa():
@@ -36,27 +62,35 @@ def run_alexa():
     # print(command)
     if 'play' in command:
         song=command.replace('play','')
+        print(f'Alexa said: Playing{song}\n')
         talk('playing' + song)
         pywhatkit.playonyt(song)
+        quit()
     elif 'time' in command:
         time=datetime.datetime.now().strftime('%I:%M %p')
-        print(time)
+        print('Alexa said: '+time)
         talk("current time is " + time)
     elif 'wiki' in command:
         object=command.replace('wiki','')
         info=wikipedia.summary(object,2)
-        print(info)
+        print('Alexa said: '+info)
         talk(info)
     elif 'joke' in command:
         joke=pyjokes.get_joke()
-        print(joke)
+        print('Alexa said: '+joke)
         talk(joke)
+    elif 'open youtube' in command:
+        webbrowser.open("youtube.com")
     elif 'stop' in command:
+        print('Shutting down...')
         talk('good bye see you again')
         quit()
     else:
+        print('Alexa said: Please say the command again')
         talk('please say the command again')
-    
+
+
+print('Initializing Alexa...')   
+wishme()
 while True:
- run_alexa()
- 
+    run_alexa()
